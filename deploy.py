47938b5fb5efe8c54127a02db517126e4f3264b8
John Lin <johnlinp@gmail.com>
2018-04-18 10:28:47 +0000

    Rewrite deploy.py

--- deploy.py
+++ deploy.py
@@ -6,100 +6,98 @@ import shutil
 import glob
 
 
+def list_entries():
+    black_list = ['deploy.py', 'README.md', 'backup']
+    entries = glob.glob('*')
+    return [entry for entry in entries if entry not in black_list]
+
+
 def link_entry(entry):
-	os.symlink(os.path.abspath(entry), get_dst_path(entry))
-
-
-def is_ignored(entry):
-	src_basename = os.path.basename(entry)
-	return src_basename in ['deploy.py', 'README.md']
+    os.symlink(os.path.abspath(entry), get_dst_path(entry))
 
 
 def get_dst_path(entry):
-	home_dir = os.path.expanduser('~')
-	return os.path.join(home_dir, '.' + entry)
+    home_dir = os.path.expanduser('~')
+    return os.path.join(home_dir, '.' + entry)
 
 
 def is_dst_exist(entry):
-	return os.path.exists(get_dst_path(entry))
+    return os.path.exists(get_dst_path(entry))
 
 
 def is_dst_same_file(entry):
-	return os.path.samefile(entry, get_dst_path(entry))
+    return os.path.samefile(entry, get_dst_path(entry))
 
 
 def get_backup_dir():
-	return os.path.abspath('backup')
+    return os.path.abspath('backup')
+
+
+def get_backup_path(entry):
+    backup_dir = get_backup_dir()
+    return os.path.join(backup_dir, entry)
 
 
 def backup_dst(entry):
-	backup_dir = get_backup_dir()
-	if not os.path.exists(backup_dir):
-		os.mkdir(backup_dir)
-	backup_full_path = os.path.join(backup_dir, entry)
-	backup_dirname = os.path.dirname(backup_full_path)
-	if not os.path.exists(backup_dirname):
-		os.makedirs(backup_dirname)
-	dst_path = get_dst_path(entry)
-	shutil.copyfile(dst_path, os.path.join(backup_dir, entry))
-	os.remove(dst_path)
+    backup_full_path = get_backup_path(entry)
+    backup_dirname = os.path.dirname(backup_full_path)
+    if not os.path.exists(backup_dirname):
+        os.makedirs(backup_dirname)
+    dst_path = get_dst_path(entry)
+    shutil.copyfile(dst_path, backup_full_path)
+    os.remove(dst_path)
 
 
 def is_already_deployed(entry):
-	if not is_dst_exist(entry):
-		return False
-	return is_dst_same_file(entry)
+    if not is_dst_exist(entry):
+        return False
+    return is_dst_same_file(entry)
 
 
-def deploy_single(entry, start, log):
-	if is_ignored(entry):
-		return
+def deploy_single(entry):
+    if not os.path.exists(entry):
+        raise Exception('no such file: {}'.format(get_dst_path(entry)))
 
-	if not os.path.exists(entry):
-		raise Exception('no such file: ' + entry)
-	elif os.path.isfile(entry):
-		if is_already_deployed(entry):
-			return
-		if is_dst_exist(entry):
-			print('backup', entry)
-			log['backup'] += 1
-			backup_dst(entry)
-		print('deploy', entry)
-		link_entry(entry)
-		log['deploy'] += 1
-	elif os.path.isdir(entry):
-		if not is_dst_exist(entry):
-			return
-		entries = glob.glob(os.path.join(entry, '*'))
-		deploy_many(entries, entry, log)
-	else:
-		raise Exception('unsupported file type: ' + entry)
+    if os.path.isfile(entry):
+        if is_already_deployed(entry):
+            print('    already deployed: {}'.format(get_dst_path(entry)))
+            return
+        if is_dst_exist(entry):
+            print('    making backup: {} -> {}'.format(get_dst_path(entry), get_backup_path(entry)))
+            backup_dst(entry)
+        print('    deploying: {}'.format(get_dst_path(entry)))
+        link_entry(entry)
+    elif os.path.isdir(entry):
+        if not is_dst_exist(entry):
+            print('    directory does not exist: {}, do not deploy'.format(get_dst_path(entry)))
+            return
+        entries = glob.glob(os.path.join(entry, '*'))
+        for entry_ in entries:
+            deploy_single(entry_)
+    else:
+        raise Exception('unsupported file type: {}'.format(get_dst_path(entry)))
 
 
-def deploy_many(entries, start, log):
-	for entry in entries:
-		deploy_single(entry, start, log)
+def deploy_many(entries):
+    for entry in entries:
+        print('deploying entry {}:'.format(entry))
+        deploy_single(entry)
 
 
 def main(argv):
-	if len(argv) == 1:
-		print('usage:')
-		print('    ./deploy.py all')
-		print('    ./deploy.py <config1> <config2> <config3> ...')
-		return
-	elif argv[1] == 'all':
-		entries = glob.glob('*')
-	else:
-		entries = argv[1:]
+    if len(argv) == 1:
+        print('usage:')
+        print('    ./deploy.py all')
+        print('    ./deploy.py <config1> <config2> <config3> ...')
+        return
 
-	log = {'deploy': 0, 'backup': 0}
-	deploy_many(entries, '', log)
+    if argv[1] == 'all':
+        entries = list_entries()
+    else:
+        entries = argv[1:]
 
-	if not log['deploy']:
-		print('nothing happened')
-	if log['backup']:
-		print('the backup files are in', get_backup_dir())
+    deploy_many(entries)
 
 
 if __name__ == '__main__':
-	main(sys.argv)
+    main(sys.argv)
