6761be575f7aa81de25d62c23c25439be7ae6ca2
John Lin <johnlinp@gmail.com>
2018-09-14 11:39:11 +0800

    Update bash auto completion paths

--- bash/.bashrc.d/auto-completion.bash
+++ bash/.bashrc.d/auto-completion.bash
@@ -1,4 +1,12 @@
-if [ -f /etc/bash_completion ]
-then
-    source /etc/bash_completion
-fi
+BASH_COMPLETION_PATHS="
+    /etc/bash_completion
+    /usr/local/etc/bash_completion
+"
+
+for P in $BASH_COMPLETION_PATHS
+do
+    if [ -f $P ]
+    then
+        source $P
+    fi
+done
